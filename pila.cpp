b7b54a95ffee2ec4c390ab2cd50b8213e8b8ed1c
ricardovosahlounsta-cloud <ricardo@unsta.com>
2026-05-22 23:41:40 +0000

    Sin comentarios

--- pila.cpp
+++ pila.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 using namespace std;
 
-// tamano maximo de la pila
 #define N 10
 
-// la pila es un arreglo simple
 int S[N];
 int tope = 0;
 
-// procedimiento AGREGAR - agrega un elemento a la pila
 void AGREGAR(int x) {
     if (tope == N) {
-        cout << "Error: la pila esta LLENA" << endl;
+        cout << "La pila esta LLENA" << endl;
         return;
     }
     tope = tope + 1;
     S[tope] = x;
 }
 
-// procedimiento ELIMINAR - elimina un elemento de la pila y lo guarda en x
 void ELIMINAR(int &x) {
     if (tope <= 0) {
-        cout << "Error: la pila esta VACIA" << endl;
+        cout << "La pila esta VACIA" << endl;
         return;
     }
     x = S[tope];
@@ -31,18 +27,13 @@ void ELIMINAR(int &x) {
 int main() {
     int x;
 
-    // agregamos algunos numeros a la pila
-    cout << "Agregando 5, 10, 15 a la pila..." << endl;
     AGREGAR(5);
     AGREGAR(10);
     AGREGAR(15);
 
-    // eliminamos un elemento
-    cout << "Eliminando un elemento de la pila..." << endl;
     ELIMINAR(x);
     cout << "Elemento eliminado: " << x << endl;
 
-    // eliminamos otro
     ELIMINAR(x);
     cout << "Elemento eliminado: " << x << endl;
 
